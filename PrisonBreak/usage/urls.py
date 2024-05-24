a729038fda0ce4218c5d1838891fa3c484962afd
Juni <kjuni1914@gmail.com>
2024-05-24 21:19:31 +0900

    background reset

--- PrisonBreak/usage/urls.py
+++ PrisonBreak/usage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.seat, name='seat'),
+    path('seat/', views.seat, name='seat'),
     path('', views.index, name='index'),
     path('login/', views.user_login, name='user_login'),
     path('createAccount/',views.create_account, name='create_account')
