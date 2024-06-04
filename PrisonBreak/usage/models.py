a97ea0cefc92b564c2d057dd9bd05a5f6cd57e4e
Juni <kjuni1914@gmail.com>
2024-06-04 17:45:02 +0900

    change

--- PrisonBreak/usage/models.py
+++ PrisonBreak/usage/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    is_confirmed = models.BooleanField(default=False)
+# class Reservation(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     start_time = models.TimeField()
+#     end_time = models.TimeField()
+#     is_confirmed = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.user} {self.date} {self.start_time}-{self.end_time}"
+#     def __str__(self):
+#         return f"{self.user} {self.date} {self.start_time}-{self.end_time}"
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
